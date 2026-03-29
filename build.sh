bd6c77e17532fa79f6b1e5bcc5e45a5c92e7e6fe
yosbelm <yosbelmc22@gmail.com>
2026-03-29 14:32:21 -0400

    actualizado el build

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r backend/requirements.txt
+
+# Convert static asset files
+python backend/manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python backend/manage.py migrate
+
+
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='yosbel').exists():
+    User.objects.create_superuser('yosbel', 'yosbel@example.com', '1234.2902Amayah')
+"
