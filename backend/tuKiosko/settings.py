4e57e8fc51b67556ef157e3762b485f581085304
yosbelm <yosbelmc22@gmail.com>
2026-03-29 14:58:35 -0400

    cors allowed origins try1

--- backend/tuKiosko/settings.py
+++ backend/tuKiosko/settings.py
@@ -148,7 +148,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  
     "http://localhost:5173",
-    "https://tukiosko-frontend.onrender.com/"
+    "https://tukiosko-frontend.onrender.com"
 ]
 
 
