6d909905df6a294c7c764e57a2cf5f512d915881
yosbelm <yosbelmc22@gmail.com>
2026-03-29 14:13:39 -0400

    ajustes en settings sobre archivos estaticos

--- backend/tuKiosko/settings.py
+++ backend/tuKiosko/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,6 +131,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Opcional pero recomendado para que Django encuentre tus archivos locales:
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
