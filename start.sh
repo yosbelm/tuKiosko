1081b357983f06c169e53c74740a4c1ac8c5dc17
yosbelm <yosbelmc22@gmail.com>
2026-08-30 19:46:32 -0400

    feat: agregado db en docker compose, fix: eliminado archivos inncesarios, arreglada la versiond e python en dockerfile

--- start.sh
+++ start.sh
@@ -9,8 +9,16 @@ python backend/manage.py migrate
 python backend/manage.py shell -c "
 from django.contrib.auth import get_user_model;
 User = get_user_model();
+
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
 if not User.objects.filter(username='yosbel').exists():
-    User.objects.create_superuser('yosbel', 'yosbel@example.com', '1234.2902Amayah')
+    User.objects.create_superuser(username, email, password)
+    print('Superusuario creado exitosamente.')
+else:
+    print('Faltan variables de entorno para el superusuario.')
 "
 
 # 3. Iniciar el servidor (Gunicorn) indicando el directorio correcto
